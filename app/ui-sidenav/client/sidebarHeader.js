4f1ae427098f0cb20f7c61f907ce9edd260561e2
badaev <b2009mv@yandex.ru>
2020-10-15 08:57:38 +0800

    RocketChat#722 [NEW] Добавлена сетка иконок на главную страницу

--- app/ui-sidenav/client/sidebarHeader.js
+++ app/ui-sidenav/client/sidebarHeader.js
@@ -36,11 +36,13 @@ export const toolbarSearch = {
 	},
 };
 
-const toolbarButtons = (/* user */) => [{
+const toolbarButtons = (/* user */) => [
+	{
 	name: t('Home'),
 	icon: 'home',
 	condition: () => settings.get('Layout_Show_Home_Button'),
 	action: () => {
+		menu.close();
 		FlowRouter.go('home');
 	},
 },
@@ -52,38 +54,12 @@ const toolbarButtons = (/* user */) => [{
 	},
 },
 {
-	name: t('Directory'),
-	icon: 'discover',
-	action: () => {
-		menu.close();
-		FlowRouter.go('directory');
-	},
-},
-{
 	name: t('Send_email'),
 	icon: 'mail',
 	condition: () => hasPermission('send-mail-manually'),
 	action: () => {
 		menu.close();
 		FlowRouter.go('manual-mail-sender');
-	},
-},
-{
-	name: t('Councils'),
-	icon: 'team',
-	condition: () => hasPermission('manage-councils'),
-	action: () => {
-		menu.close();
-		FlowRouter.go('councils');
-	},
-},
-{
-	name: t('Working_group'),
-	icon: 'team',
-	condition: () => hasPermission('manage-working-group'),
-	action: () => {
-		menu.close();
-		FlowRouter.go('working-group');
 	},
 },
 {
@@ -100,139 +76,6 @@ const toolbarButtons = (/* user */) => [{
 			},
 			offsetVertical: e.currentTarget.clientHeight + 10,
 		};
-		popover.open(config);
-	},
-},
-{
-	name: t('Create_new'),
-	icon: 'edit-rounded',
-	condition: () => hasAtLeastOnePermission(['create-c', 'create-p', 'create-d', 'start-discussion', 'start-discussion-other-user']),
-	hasPopup: true,
-	action: (e) => {
-		const action = (title, content) => (e) => {
-			e.preventDefault();
-			modal.open({
-				title: t(title),
-				content,
-				data: {
-					onCreate() {
-						modal.close();
-					},
-				},
-				modifier: 'modal',
-				showConfirmButton: false,
-				showCancelButton: false,
-				confirmOnEnter: false,
-			});
-		};
-
-		const createChannel = action('Create_A_New_Channel', 'createChannel');
-		const createDirectMessage = action('Direct_Messages', 'CreateDirectMessage');
-		const createDiscussion = action('Discussion_title', 'CreateDiscussion');
-
-
-		const items = [
-			hasAtLeastOnePermission(['create-c', 'create-p'])
-			&& {
-				icon: 'hashtag',
-				name: t('Channel'),
-				action: createChannel,
-			},
-			hasPermission('create-d')
-			&& {
-				icon: 'team',
-				name: t('Direct_Messages'),
-				action: createDirectMessage,
-			},
-			settings.get('Discussion_enabled') && hasAtLeastOnePermission(['start-discussion', 'start-discussion-other-user'])
-			&& {
-				icon: 'discussion',
-				name: t('Discussion'),
-				action: createDiscussion,
-			},
-		].filter(Boolean);
-
-		if (items.length === 1) {
-			return items[0].action(e);
-		}
-
-		const config = {
-			columns: [
-				{
-					groups: [
-						{
-							items,
-						},
-					],
-				},
-			],
-			currentTarget: e.currentTarget,
-			offsetVertical: e.currentTarget.clientHeight + 10,
-		};
-		popover.open(config);
-	},
-},
-{
-	name: t('Options'),
-	icon: 'menu',
-	condition: () => AccountBox.getItems().length || hasAtLeastOnePermission(['manage-emoji', 'manage-oauth-apps', 'manage-outgoing-integrations', 'manage-incoming-integrations', 'manage-own-outgoing-integrations', 'manage-own-incoming-integrations', 'manage-selected-settings', 'manage-sounds', 'view-logs', 'view-privileged-setting', 'view-room-administration', 'view-statistics', 'view-user-administration', 'access-setting-permissions']),
-	hasPopup: true,
-	action: (e) => {
-		let adminOption;
-		if (hasAtLeastOnePermission(['manage-emoji', 'manage-oauth-apps', 'manage-outgoing-integrations', 'manage-incoming-integrations', 'manage-own-outgoing-integrations', 'manage-own-incoming-integrations', 'manage-selected-settings', 'manage-sounds', 'view-logs', 'view-privileged-setting', 'view-room-administration', 'view-statistics', 'view-user-administration', 'access-setting-permissions'])) {
-			adminOption = {
-				icon: 'customize',
-				name: t('Administration'),
-				type: 'open',
-				id: 'administration',
-				action: () => {
-					FlowRouter.go('admin', { group: 'info' });
-					popover.close();
-				},
-			};
-		}
-
-		const config = {
-			popoverClass: 'sidebar-header',
-			columns: [
-				{
-					groups: [
-						{
-							items: AccountBox.getItems().map((item) => {
-								let action;
-
-								if (item.href || item.sideNav) {
-									action = () => {
-										if (item.href) {
-											FlowRouter.go(item.href);
-											popover.close();
-										}
-										if (item.sideNav) {
-											SideNav.setFlex(item.sideNav);
-											SideNav.openFlex();
-											popover.close();
-										}
-									};
-								}
-
-								return {
-									icon: item.icon,
-									name: t(item.name),
-									type: 'open',
-									id: item.name,
-									href: item.href,
-									sideNav: item.sideNav,
-									action,
-								};
-							}).concat([adminOption]),
-						},
-					],
-				},
-			],
-			currentTarget: e.currentTarget,
-			offsetVertical: e.currentTarget.clientHeight + 10,
-		};
-
 		popover.open(config);
 	},
 }];
