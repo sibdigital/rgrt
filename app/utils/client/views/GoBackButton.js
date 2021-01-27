7acae7d47c06eea16eac9969e8806cdbdd71d970
badaev <b2009mv@yandex.ru>
2021-01-27 17:26:47 +0800

    [EDIT] Рефакторинг кнопки "Назад"

--- app/utils/client/views/GoBackButton.js
+++ app/utils/client/views/GoBackButton.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Button, Icon } from '@rocket.chat/fuselage';
 
-export function GoBackButton() {
+export function GoBackButton({ ...props }) {
 	const goBack = () => {
 		window.history.back();
 	};
 
-	return <Button className='go-back-button' onClick={goBack}>
+	return <Button className='go-back-button' onClick={goBack} {...props}>
 		<Icon name='back'/>
 	</Button>;
 }
