3b735e902b054b191dda529cf7f699627ae4c816
badaev <b2009mv@yandex.ru>
2021-02-02 15:05:19 +0800

    [EDIT] Comments

--- app/utils/client/methods/checkNumber.js
+++ app/utils/client/methods/checkNumber.js
@@ -1,6 +1,14 @@
-// format = 2,2,3
-export const checkNumberWithDot = (value, prevValue) => {
-	if (!value || value.length < 1 || value[0] === '.') {
+/*
+	input: @value = string
+		   @prevValue = string
+
+	method retrieves the result of matching a string against a regular expression.
+
+	output: null if string not match
+			array of matches else
+*/
+export const checkNumberWithDot = (value, prevValue = '') => {
+	if (!value || typeof value !== 'string' || value.length < 1 || value[0] === '.') {
 		return null;
 	}
 	let str = value;
--- app/utils/client/methods/constructPersonFIO.js
+++ app/utils/client/methods/constructPersonFIO.js
@@ -1,12 +1,44 @@
+/*
+	input: @person - Object
+		Object format: {
+			surname: string,
+			name: string,
+			patronymic: string,
+		}
+
+	method returns short full name in format surname.n.p.
+
+	output: empty string if !person or person haven't surname, name, patronymic
+			string like surname.n.p.
+*/
 export function constructPersonFIO(person) {
-	if (!person) {
+	if (!person || typeof person !== 'object') {
+		return '';
+	}
+	if (!person.surname && !person.name && !person.patronymic) {
 		return '';
 	}
 	return [person.surname ?? '', ' ', person.name?.substr(0, 1) ?? '', '.', person.patronymic?.substr(0, 1) ?? '', '.'].join('');
 }
 
+/*
+	input: @person - Object
+		Object format: {
+			surname: string,
+			name: string,
+			patronymic: string,
+		}
+
+	method returns full name in format surname name patronymic
+
+	output: empty string if !person or person haven't surname, name, patronymic
+			string like: surname name patronymic
+*/
 export function constructPersonFullFIO(person) {
-	if (!person) {
+	if (!person || typeof person !== 'object') {
+		return '';
+	}
+	if (!person.surname && !person.name && !person.patronymic) {
 		return '';
 	}
 	return [person.surname ?? '', person.name ?? '', person.patronymic ?? ''].join(' ');
