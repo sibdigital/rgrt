3c18def7ad31cc57addfc8c176aa53b67a0504de
badaev <b2009mv@yandex.ru>
2021-02-24 10:07:47 +0800

    №952 [EDIT] Добавлен метод проверки на римские цифры

--- app/utils/client/methods/checkNumber.js
+++ app/utils/client/methods/checkNumber.js
@@ -1,3 +1,8 @@
+const validate = (value) => {
+	// console.log({ value, valid: !(!value || typeof value !== 'string') });
+	return !(!value || typeof value !== 'string');
+};
+
 /*
 	input: @value = string
 		   @prevValue = string
@@ -7,14 +12,42 @@
 	output: null if string not match
 			array of matches else
 */
-export const checkNumberWithDot = (value, prevValue = '') => {
-	if (!value || typeof value !== 'string' || value.length < 1 || value[0] === '.') {
+
+export const checkNumberWithDot = (value) => {
+	if (value === '') {
+		return value;
+	}
+
+	if (!validate(value) || value === '.') {
 		return null;
 	}
+
 	let str = value;
 	if (value[value.length - 1] === '.') {
 		str = value.slice(0, value.length - 1);
 	}
 
 	return str.match('^\\d+(\\.\\d+)*$');
+};
+
+
+/*
+	input: @value = string
+
+	method retrieves the result of matching a string against a regular expression.
+
+	output: null if string not match
+			array of matches else
+*/
+
+export const checkRomanNumber = (value) => {
+	if (value === '') {
+		return value;
+	}
+
+	if (!validate(value)) {
+		return null;
+	}
+
+	return value.match('^(?=[MDCLXVI])M*(C[MD]|D?C{0,3})(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$');
 };
