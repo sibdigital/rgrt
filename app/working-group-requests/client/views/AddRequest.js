01812128c1f494882e2ec447b10bfd50985b4662
eugene <poebot3r@gmail.com>
2021-03-16 17:14:20 +0800

    Проверка на council._id

--- app/working-group-requests/client/views/AddRequest.js
+++ app/working-group-requests/client/views/AddRequest.js
@@ -225,7 +225,7 @@ function NewAddRequest({ mode, request, onChange, onRequestChanged, docsdata, ..
 			console.log(protocol);
 			if (protocol) {
 				if (protocol.protocol) {
-					setCouncilId(protocol.protocol[0]?.councilId);
+					setCouncilId(protocol.protocol[0]?.council?._id);
 				}
 				if (protocol.sections) {
 					const protocolItem = protocol.sections.map(section => section.items.filter(item => item._id === protocolsItemId)[0])[0];
@@ -236,7 +236,7 @@ function NewAddRequest({ mode, request, onChange, onRequestChanged, docsdata, ..
 					setProtocol({ d: protocol.protocol[0]?.d, num: protocol.protocol[0]?.num,  itemNum: protocolItem.num, itemResponsible: itemResponsiblePerson})
 				}
 				if (protocol.protocol && council) {
-					const protocolCouncilId = protocol.protocol[0]?.councilId;
+					const protocolCouncilId = protocol.protocol[0]?.council?._id;
 					const councilData = council?.councils?.filter(i => i._id === protocolCouncilId);
 					setCouncil({ d: councilData[0]?.d, desc: councilData[0]?.desc });
 				}
