c8dc56c34ed68c8fe5f89f87d4c13f3a460d83d9
badaev <b2009mv@yandex.ru>
2021-04-02 16:38:41 +0800

    №951 [EDIT] Фильтр поиска ответственного

--- app/working-group-requests/client/views/ResponsibleChoose.js
+++ app/working-group-requests/client/views/ResponsibleChoose.js
@@ -1,11 +1,12 @@
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
-import { Box } from '@rocket.chat/fuselage';
-import { useDebouncedValue } from '@rocket.chat/fuselage-hooks';
+import { Box, Field, Icon, TextInput } from '@rocket.chat/fuselage';
+import { useDebouncedValue, useMutableCallback } from '@rocket.chat/fuselage-hooks';
+import { css } from '@rocket.chat/css-in-js';
 
 import { constructPersonFullFIO } from '../../../utils/client/methods/constructPersonFIO';
 import { GenericList } from '../../../../client/components/GenericList';
 import { useEndpointDataExperimental } from '../../../../client/hooks/useEndpointDataExperimental';
-import { css } from '@rocket.chat/css-in-js';
+import { useTranslation } from '../../../../client/contexts/TranslationContext';
 
 const clickable = css`
 		cursor: pointer;
@@ -15,15 +16,52 @@ const clickable = css`
 		}
 	`;
 
+const FilterByText = ({ setFilter, ...props }) => {
+	const t = useTranslation();
+	const [text, setText] = useState('');
+
+	const handleChange = useMutableCallback((event) => setText(event.currentTarget.value));
+	const onSubmit = useMutableCallback((e) => e.preventDefault());
+
+	useEffect(() => {
+		setFilter({ text, offset: 0, current: 0 });
+	}, [setFilter, text]);
+
+	return <Box mb='x16' is='form' onSubmit={onSubmit} display='flex' flexDirection='column' {...props}>
+		<Field.Label>{t('Search')}</Field.Label>
+		<TextInput flexShrink={0} placeholder={t('Search')} addon={<Icon name='magnifier' size='x20'/>} onChange={handleChange} value={text} />
+	</Box>;
+};
+
 const sortDir = (sortDir) => (sortDir === 'asc' ? 1 : -1);
 
-const useQuery = ({ itemsPerPage, current }, [column, direction], personFields, prevResponsiblesId) => useMemo(() => ({
-	query: JSON.stringify({ _id: { $ne: { $in: { prevResponsiblesId } } } }),
+const useQuery = ({ text, itemsPerPage, current }, [column, direction], personFields, prevResponsiblesId) => useMemo(() => ({
+	query: JSON.stringify({
+		$or: [{
+			surname: { $regex: text || '', $options: 'i' },
+			name: { $regex: text || '', $options: 'i' },
+			patronymic: { $regex: text || '', $options: 'i' },
+			email: { $regex: text || '', $options: 'i' },
+		}],
+		// $and: [
+		// 	{
+		// 		_id: { $ne: { $in: { prevResponsiblesId } } },
+		// 	},
+		// 	{
+		// 		$or: [{
+		// 			surname: { $regex: text || '', $options: 'i' },
+		// 			name: { $regex: text || '', $options: 'i' },
+		// 			patronymic: { $regex: text || '', $options: 'i' },
+		// 			email: { $regex: text || '', $options: 'i' },
+		// 		}],
+		// 	},
+		// ],
+	}),
 	fields: JSON.stringify(personFields),
 	sort: JSON.stringify({ [column]: sortDir(direction) }),
 	...itemsPerPage && { count: itemsPerPage },
 	...current && { offset: current },
-}), [prevResponsiblesId, personFields, column, direction, itemsPerPage, current]);
+}), [text, prevResponsiblesId, personFields, column, direction, itemsPerPage, current]);
 
 export function ResponsibleChoose({
 	onSetResponsible,
@@ -31,10 +69,11 @@ export function ResponsibleChoose({
 	close,
 	personFields = null,
 }) {
-	const [params, setParams] = useState({ current: 0, itemsPerPage: 25 });
+	const [params, setParams] = useState({ current: 0, itemsPerPage: 25, text: '' });
 	const [sort, setSort] = useState(['surname']);
 	const [refProtocolsFields, setRefProtocolsFields] = useState(personFields ?? { surname: 1, name: 1, patronymic: 1, userId: 1 });
 	const [refPrevResponsiblesId, setRefPrevResponsiblesId] = useState(prevResponsiblesId ?? []);
+	const [loadedMore, setLoadedMore] = useState(false);
 
 	const [personsList, setPersonsList] = useState([]);
 
@@ -47,10 +86,15 @@ export function ResponsibleChoose({
 
 	useEffect(() => {
 		if (personData && personData.persons) {
-			console.dir({ personData, personsList });
-			setPersonsList(personsList.concat(personData.persons));
+			console.dir({ personData, personsList, debouncedParams });
+			if (debouncedParams.current > 0) {
+				setPersonsList(personsList.concat(personData.persons));
+			} else {
+				setPersonsList(personData.persons);
+			}
 		}
 	}, [personData]);
+	useMemo(() => console.dir({ params }), [params]);
 
 	const handleChoose = useCallback((responsible) => {
 		onSetResponsible && onSetResponsible(responsible);
@@ -73,5 +117,5 @@ export function ResponsibleChoose({
 		</Box>;
 	};
 
-	return <GenericList layout={'column'} renderRow={renderRow} results={personsList} total={personData?.total ?? 0} setParams={setParams} params={params}/>;
+	return <GenericList onLoadMore={() => setLoadedMore(true)} FilterComponent={FilterByText} layout={'column'} renderRow={renderRow} results={personsList} total={personData?.total ?? 0} setParams={setParams} params={params}/>;
 }
