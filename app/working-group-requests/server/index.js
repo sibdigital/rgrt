bcdbd9e2faa1760141742a7c4fd1a1a5a5c94e0b
badaev <b2009mv@yandex.ru>
2021-04-14 16:25:45 +0800

    №1037 [EDIT] requests delete

--- app/working-group-requests/server/index.js
+++ app/working-group-requests/server/index.js
@@ -2,3 +2,4 @@ import './methods/insertOrUpdateWorkingGroupRequest';
 import './methods/addWorkingGroupRequestAnswer';
 import './methods/readAnswer';
 import './methods/insertOrUpdateWorkingGroupRequestMail';
+import './methods/deleteWorkingGroupRequest';
