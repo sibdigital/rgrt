4cadea54fa78cfefdafe5033c363f7fc9fa97fac
badaev <b2009mv@yandex.ru>
2021-04-16 13:30:44 +0800

    №1037 [EDIT] Фикс просмотра ответа

--- app/working-group-requests/server/methods/readAnswer.js
+++ app/working-group-requests/server/methods/readAnswer.js
@@ -1,21 +1,13 @@
 import { Meteor } from 'meteor/meteor';
 
-import { WorkingGroupsRequests } from '../../../models/server';
+import { Errands } from '../../../models/server';
 
 Meteor.methods({
-	readAnswer(_id, answerId) {
+	readAnswer(_id) {
 		if (!_id) {
 			throw new Meteor.Error('error-the-field-is-required', 'The field id is required', { method: 'readAnswer', field: 'id' });
 		}
 
-		// if (!mailId) {
-		// 	throw new Meteor.Error('error-the-field-is-required', 'The field mailId is required', { method: 'readAnswer', field: 'mailId' });
-		// }
-
-		if (!answerId) {
-			throw new Meteor.Error('error-the-field-is-required', 'The field answerId is required', { method: 'readAnswer', field: 'answerId' });
-		}
-
-		return WorkingGroupsRequests.readAnswer(_id, answerId);
+		return Errands.readAnswer(_id);
 	},
 });
