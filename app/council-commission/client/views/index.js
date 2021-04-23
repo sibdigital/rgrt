812ef1824546a9bfa958f5079fc30c491b98671a
badaev <b2009mv@yandex.ru>
2021-04-23 17:14:54 +0800

    подргузка физ.лиц в составе комисии

--- app/council-commission/client/views/index.js
+++ app/council-commission/client/views/index.js
@@ -1,6 +1,7 @@
-import React, { useCallback, useMemo, useState, useEffect } from 'react';
+import React, { ReactDOM, useMemo, useState, useEffect, useCallback } from 'react';
 import { ButtonGroup, Button, Field, Box, Label, Icon } from '@rocket.chat/fuselage';
 import { useDebouncedValue, useMediaQuery } from '@rocket.chat/fuselage-hooks';
+import ScrollListener from 'react-scroll-listener';
 
 import Page from '../../../../client/components/basic/Page';
 import { useTranslation } from '../../../../client/contexts/TranslationContext';
@@ -9,6 +10,7 @@ import VerticalBar from '../../../../client/components/basic/VerticalBar';
 import { useEndpointDataExperimental } from '../../../../client/hooks/useEndpointDataExperimental';
 import { GoBackButton } from '../../../utils/client/views/GoBackButton';
 import { useEndpointData } from '/client/hooks/useEndpointData';
+import { isBrowser } from 'react-device-detect';
 
 const sortDir = (sortDir) => (sortDir === 'asc' ? 1 : -1);
 
@@ -23,10 +25,12 @@ const useQueryPerson = ({ itemsPerPage, current }, [column, direction]) => useMe
 export function CouncilCommission() {
 	const routeName = 'council-commission';
 
-	const [persons, setPersons] = useState([]) ;
-    const [params, setParams] = useState({ current: 0, itemsPerPage: 25 });
+	const [persons, setPersons] = useState([]);
+	const [params, setParams] = useState({ current: 0, itemsPerPage: 25 });
 	const [sort, setSort] = useState(['weight']);
-	const debouncedParams = useDebouncedValue(params, 500);	
+	const [isEndScroll, setIsEndScroll] = useState(false);
+
+	const debouncedParams = useDebouncedValue(params, 500);
 	const debouncedSort = useDebouncedValue(sort, 500);
 
 	const queryPerson = useQueryPerson(debouncedParams, debouncedSort);
@@ -39,44 +43,60 @@ export function CouncilCommission() {
 		}
 	}, [personsData]);
 
-	return <CouncilCommissionPage persons={persons}/>
+	const handleScroll = useCallback(() => {
+		console.dir({ params });
+		if (isEndScroll && personsData?.total > params.current + params.itemsPerPage) {
+			setParams({ ...params, current: params?.current + params?.itemsPerPage });
+			setIsEndScroll(false);
+		}
+	}, [isEndScroll, params, personsData?.total]);
+
+	useMemo(() => handleScroll(), [isEndScroll]);
+
+	return <CouncilCommissionPage persons={persons} setIsEndScroll={setIsEndScroll}/>;
 }
 
-export function CouncilCommissionPage(
-	persons
-) {
-    const t = useTranslation();
-
-	const [cache, setCache] = useState();
+function CouncilCommissionPage({
+	persons,
+	setIsEndScroll,
+}) {
+	const t = useTranslation();
 
 	const mediaQuery = useMediaQuery('(min-width: 559px)');
 
-	const RenderBox = ({person, index}) => {
+	const handleScroll = useCallback((e) => {
+		console.dir({ e });
+		if (e.bottom) {
+			setIsEndScroll(true);
+		}
+	}, [setIsEndScroll]);
+
+	const RenderBox = ({ person, index }) => {
 		const { _id, name, surname, patronymic, email, phone, organization, position, username, avatarSource} = person;
 
 		if (index === 0) {
-			return <Box className={`commission-person-block--${index}`} position='relative'>
-			<img width='100%' height='100%' className='imgRerenderer' src={avatarSource?.url}/>
-			<Box className='imgSide-bg gradient' w='100%' >
-				<Box className='imgSide-inf'>
-					<Box fontSize={mediaQuery ? 'x32' : 'x24'}>
-						<Box>{surname}</Box>
-						<Box>{name} {patronymic}</Box>
-					</Box>
-					<Box fontSize={mediaQuery ? 'x18' : 'x16'} mbs='x16' lineHeight={mediaQuery ? 'x24' : 'x18'}>
-						<Box>{position}</Box>
-						<Box>{organization}</Box>
-					</Box>
-					<Box fontSize={mediaQuery ? 'x18' : 'x16'} mbs='x12' lineHeight={mediaQuery ? 'x24' : 'x18'}>
-						<Box>{phone}</Box>
-						<Box style={{wordBreak:'break-word'}}>{email}</Box>
+			return <Box className={`commission-person-block--${ index }`} position='relative'>
+				<img alt={t('Avatar')} width='100%' height='100%' className='imgRerenderer' src={avatarSource?.url}/>
+				<Box className='imgSide-bg gradient' w='100%' >
+					<Box className='imgSide-inf'>
+						<Box fontSize={mediaQuery ? 'x32' : 'x24'}>
+							<Box>{surname}</Box>
+							<Box>{name} {patronymic}</Box>
+						</Box>
+						<Box fontSize={mediaQuery ? 'x18' : 'x16'} mbs='x16' lineHeight={mediaQuery ? 'x24' : 'x18'}>
+							<Box>{position}</Box>
+							<Box>{organization}</Box>
+						</Box>
+						<Box fontSize={mediaQuery ? 'x18' : 'x16'} mbs='x12' lineHeight={mediaQuery ? 'x24' : 'x18'}>
+							<Box>{phone}</Box>
+							<Box style={{ wordBreak: 'break-word' }}>{email}</Box>
+						</Box>
 					</Box>
 				</Box>
-			</Box>
-		</Box>;
+			</Box>;
 		}
-		return  <Box className={`commission-person-block`} display='flex'>
-			<Box flexBasis='40%'><img width='100%' height='100%' className='imgRerenderer' src={avatarSource?.url}/></Box>
+		return <Box className={'commission-person-block'} display='flex'>
+			<Box flexBasis='40%'><img alt={t('Avatar')} width='100%' height='100%' className='imgRerenderer' src={avatarSource?.url}/></Box>
 			<Box className='right-elem'>
 				<Box className='fio'>
 					<Box>{surname} {name}</Box>
@@ -94,6 +114,8 @@ export function CouncilCommissionPage(
 		</Box>;
 	};
 
+	// useMemo(() => console.dir({ persons, type: typeof setIsEndScroll, typeC: typeof persons }), [persons, setIsEndScroll]);
+
 	return <Page flexDirection='row'>
 		<Page>
 			<Page.Header display={mediaQuery ? 'flex' : 'block'}>
@@ -102,19 +124,17 @@ export function CouncilCommissionPage(
 					<Label fontScale='h1'>{t('Commission of the State Council of the Russian Federation in the direction of \"Transport\"')}</Label>
 				</Field>
 			</Page.Header>
-			<Page.ScrollableContent>
-				<Box className={'council-commission-grid'}>
-				{/* display={mediaQuery ? 'flex' : 'unset'} flexWrap={'wrap'} justifyContent={'flex-start'} maxWidth='x1800' w='full' alignSelf='center' fontSize='x16' */}
-					{persons 
-						? persons.persons.map((person, index) => 
-						<RenderBox 
-						key={person._id || index} 
-						person={person}
-						index={index}
-						/>)
+			<Page.ScrollableContent onScrollContent={(e) => handleScroll(e)}>
+				<Box className={'council-commission-grid'} >
+					{persons
+						? persons.map((person, index) =>
+							<RenderBox
+								key={person._id || index}
+								person={person}
+								index={index}
+							/>)
 						: <Box/>}
 				</Box>
-				{/* <PersonsTable setParam={setParams} params={params}  personsData={persons} onEditClick={onEditClick} sort={sort}/> */}
 			</Page.ScrollableContent>
 		</Page>
 	</Page>;
